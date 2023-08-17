cbef9fd6d8df62348e7417c4d48859c06d80d744
hmkimm <lisamin7@naver.com>
2023-08-18 03:15:28 +0900

    Chore: 오타 수정

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-import setupColaGenerator from "./classes/colaGenerator.js";
+import ColaGenerator from "./classes/colaGenerator.js";
 import VendingMachineEvents from "./classes/vendingMachineEvents.js";
 
+const colaGenerator = new ColaGenerator();
 const vendingMachineEvents = new VendingMachineEvents();
 
 
 (async function () {
-    await setupColaGenerator();
+    await colaGenerator.setup();
     vendingMachineEvents.bindEvent();
 })()
