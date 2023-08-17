cd9608cd8336aadfe4ce7405da317e32957738fc
hmkimm <lisamin7@naver.com>
2023-08-18 02:53:43 +0900

    Style: 불필요한 코드 삭제

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
-import ColaGenerator from "./classes/colaGenerator.js";
+import setupColaGenerator from "./classes/colaGenerator.js";
 import VendingMachineEvents from "./classes/vendingMachineEvents.js";
 
-const colaGenerator = new ColaGenerator();
 const vendingMachineEvents = new VendingMachineEvents();
 
-// 탑레벨 await : 최상위 모듈에서 실핼되는 await
 
 (async function () {
-    await colaGenerator.setup();
+    await setupColaGenerator();
     vendingMachineEvents.bindEvent();
 })()
-console.log(colaGenerator)
-console.log(colaGenerator.itemList)
-console.log(colaGenerator.loadData())
